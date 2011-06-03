0bff4073747b88aa4672ee437f8ee06e1ca93041
mapleelpam <mapleelpam@gmail.com>
2011-06-03 13:04:49 +0800

    modify build.sh: add absolute path

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+BINARY_PATH=$(dirname $0)/bin/
 BUILD_FORLDER=build_tmp
 
 rm -rf ${BUILD_FORLDER}
 mkdir ${BUILD_FORLDER}
 cd ${BUILD_FORLDER}
 
+export PATH=${BINARY_PATH}:${PATH}
 cmake ..
-#make asc.jar pnc
-make pnc
+make asc.jar pnc
+make runtest-complex
